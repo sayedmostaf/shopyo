42c47ffba1615e67b26a67a13f9e5f1ec0ac14d9
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-10-10 15:09:00 +0300

    trigger get category bloc with the ui

--- lib/features/admin/add_categories/presentation/refactors/add_categories_body.dart
+++ lib/features/admin/add_categories/presentation/refactors/add_categories_body.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopyo/core/common/loading/empty_screen.dart';
+import 'package:shopyo/core/common/loading/loading_shimmer.dart';
 import 'package:shopyo/core/style/colors/colors_dark.dart';
+import 'package:shopyo/features/admin/add_categories/presentation/blocs/get_all_admin_categories/get_all_admin_categories_bloc.dart';
 import 'package:shopyo/features/admin/add_categories/presentation/widgets/add_category_item.dart';
 import 'package:shopyo/features/admin/add_categories/presentation/widgets/create/create_category.dart';
 
@@ -17,26 +21,71 @@ class AddCategoriesBody extends StatelessWidget {
           Expanded(
             child: RefreshIndicator(
               color: ColorsDark.blueDark,
-              onRefresh: () async {},
+              onRefresh: () async {
+                context.read<GetAllAdminCategoriesBloc>().add(
+                  GetAllAdminCategoriesEvent.fetchAdminCategories(),
+                );
+              },
               child: CustomScrollView(
                 slivers: [
                   SliverToBoxAdapter(child: SizedBox(height: 20.h)),
                   SliverToBoxAdapter(
-                    child: ListView.separated(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        return AddCategoryItem(
-                          name: 'MacBook',
-                          image:
-                              'https://images.unsplash.com/photo-1541807084-5c52b6b3adef?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                          categoryId: "1",
-                        );
-                      },
-                      separatorBuilder: (context, index) =>
-                          SizedBox(height: 15.h),
-                      itemCount: 10,
-                    ),
+                    child:
+                        BlocBuilder<
+                          GetAllAdminCategoriesBloc,
+                          GetAllAdminCategoriesState
+                        >(
+                          builder: (context, state) {
+                            return state.when(
+                              loading: () {
+                                return ListView.separated(
+                                  shrinkWrap: true,
+                                  physics: NeverScrollableScrollPhysics(),
+                                  itemBuilder: (context, index) {
+                                    return LoadingShimmer(
+                                      height: 130.h,
+                                      borderRadius: 15,
+                                    );
+                                  },
+                                  separatorBuilder: (context, index) =>
+                                      SizedBox(height: 15.h),
+                                  itemCount: 4,
+                                );
+                              },
+                              success: (listData) {
+                                return ListView.separated(
+                                  shrinkWrap: true,
+                                  physics: NeverScrollableScrollPhysics(),
+                                  itemBuilder: (context, index) {
+                                    return AddCategoryItem(
+                                      name:
+                                          listData
+                                              .getAllCategoriesList[index]
+                                              .name ??
+                                          '',
+                                      image:
+                                          listData
+                                              .getAllCategoriesList[index]
+                                              .image ??
+                                          '',
+                                      categoryId:
+                                          listData
+                                              .getAllCategoriesList[index]
+                                              .id ??
+                                          "",
+                                    );
+                                  },
+                                  separatorBuilder: (context, index) =>
+                                      SizedBox(height: 15.h),
+                                  itemCount:
+                                      listData.getAllCategoriesList.length,
+                                );
+                              },
+                              empty: EmptyScreen.new,
+                              error: Text.new,
+                            );
+                          },
+                        ),
                   ),
                 ],
               ),
