39f1704ac56336b2cb4b81018f4dc2f65adb1f0c
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-12-04 20:00:08 +0200

    get all notification item ui

--- lib/features/admin/add_notifications/presentation/refactors/add_notifications_body.dart
+++ lib/features/admin/add_notifications/presentation/refactors/add_notifications_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopyo/features/admin/add_notifications/presentation/widgets/add_notification_item.dart';
 import 'package:shopyo/features/admin/add_notifications/presentation/widgets/create/create_notification.dart';
 
 class AddNotificationsBody extends StatelessWidget {
@@ -9,7 +10,30 @@ class AddNotificationsBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 30.h),
-      child: Column(children: [CreateNotification()]),
+      child: Column(
+        children: [
+          CreateNotification(),
+          Expanded(
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(child: SizedBox(height: 20.h)),
+                SliverToBoxAdapter(
+                  child: ListView.separated(
+                    itemBuilder: (context, index) {
+                      return AddNotificationItem();
+                    },
+                    separatorBuilder: (context, index) =>
+                        SizedBox(height: 15.h),
+                    itemCount: 3,
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/features/admin/add_notifications/presentation/widgets/add_notification_item.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:retrofit/http.dart';
+import 'package:shopyo/core/common/widgets/custom_container_linear_admin.dart';
+import 'package:shopyo/core/common/widgets/text_app.dart';
+import 'package:shopyo/core/extensions/context_extension.dart';
+import 'package:shopyo/core/style/colors/colors_dark.dart';
+import 'package:shopyo/core/style/fonts/font_family_helper.dart';
+import 'package:shopyo/core/style/fonts/font_weight_helper.dart';
+
+class AddNotificationItem extends StatelessWidget {
+  const AddNotificationItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomContainerLinearAdmin(
+      height: 180.h,
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 15.h),
+        child: Column(
+          children: [
+            Spacer(),
+            NotificationInfo(title: 'Title: ', body: 'viewafd'),
+            Spacer(),
+            NotificationInfo(title: 'Body: ', body: 'body adsfdfafad'),
+            Spacer(),
+            NotificationInfo(title: 'Created At: ', body: '17-2-2024'),
+            Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Icons.delete, color: Colors.red),
+                SizedBox(width: 40.w),
+                Icon(Icons.edit, color: Colors.yellow),
+                SizedBox(width: 40.w),
+                Icon(Icons.send, color: Colors.green),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NotificationInfo extends StatelessWidget {
+  const NotificationInfo({super.key, required this.title, required this.body});
+  final String title, body;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        TextApp(
+          text: title,
+          theme: context.textStyle.copyWith(
+            fontSize: 14.sp,
+            color: Colors.white,
+            fontFamily: FontFamilyHelper.poppinsEnglish,
+            fontWeight: FontWeightHelper.medium,
+          ),
+        ),
+        SizedBox(height: 8.w),
+
+        TextApp(
+          text: body,
+          theme: context.textStyle.copyWith(
+            fontSize: 14.sp,
+            color: ColorsDark.blueLight,
+            fontFamily: FontFamilyHelper.poppinsEnglish,
+            fontWeight: FontWeightHelper.medium,
+          ),
+        ),
+      ],
+    );
+  }
+}
