e3cf62b67ecdf0f3f354b612d65c6ddde5017e92
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-12-07 18:48:54 +0200

    bottom nav bar icon

---
+++ lib/features/customer/main/presentation/refactors/main_bottom_nav_bar.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shopyo/core/common/animations/animate_do.dart';
+import 'package:shopyo/core/extensions/context_extension.dart';
+import 'package:shopyo/core/style/images/app_images.dart';
+import 'package:shopyo/features/customer/main/presentation/widgets/icon_tap_nav_bar.dart';
+
+class MainBottomNavBar extends StatelessWidget {
+  const MainBottomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomFadeInUp(
+      duration: 800,
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: SizedBox(
+          height: 103.h,
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  SizedBox(height: 15.h),
+                  Container(
+                    height: 88.h,
+                    color: context.color.navBarbg,
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: SizedBox(
+                        height: 45.h,
+                        width: 300.w,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 20.w),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              IconTapNavBar(
+                                onTap: () {},
+                                icon: AppImages.homeTab,
+                                isSelected: true,
+                              ),
+                              IconTapNavBar(
+                                onTap: () {},
+                                icon: AppImages.categoriesTab,
+                                isSelected: true,
+                              ),
+                              IconTapNavBar(
+                                onTap: () {},
+                                icon: AppImages.favouritesTab,
+                                isSelected: true,
+                              ),
+                              IconTapNavBar(
+                                onTap: () {},
+                                icon: AppImages.profileTab,
+                                isSelected: true,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                top: -12,
+                left: -8,
+
+                child: Container(
+                  height: 100.h,
+                  width: 100.w,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(context.asset.bigNavBar!),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 35,
+                top: 17,
+                child: SvgPicture.asset(
+                  AppImages.carShop,
+                  color: Colors.white,
+                  height: 20.h,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/customer/main/presentation/screen/main_screen.dart
+++ lib/features/customer/main/presentation/screen/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shopyo/core/extensions/context_extension.dart';
+import 'package:shopyo/features/customer/main/presentation/refactors/main_bottom_nav_bar.dart';
 import 'package:shopyo/features/customer/main/presentation/refactors/main_customer_app_bar.dart';
 
 class MainScreen extends StatelessWidget {
@@ -6,6 +8,23 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: MainCustomerAppBar());
+    return Scaffold(
+      appBar: MainCustomerAppBar(),
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(context.asset.homeBg!),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(child: Text('Main Screen Body')),
+            MainBottomNavBar(),
+          ],
+        ),
+      ),
+    );
   }
 }
