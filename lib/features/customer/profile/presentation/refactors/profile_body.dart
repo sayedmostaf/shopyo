1c63b6c0e729e018edeb9816cd096d1a104d5fb0
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-12-12 19:35:49 +0200

    language settings profile done

--- lib/features/customer/profile/presentation/refactors/profile_body.dart
+++ lib/features/customer/profile/presentation/refactors/profile_body.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopyo/core/common/animations/animate_do.dart';
+import 'package:shopyo/core/common/widgets/text_app.dart';
+import 'package:shopyo/core/extensions/context_extension.dart';
+import 'package:shopyo/core/language/lang_keys.dart';
+import 'package:shopyo/core/style/fonts/font_weight_helper.dart';
 import 'package:shopyo/features/customer/profile/presentation/bloc/profile_bloc/profile_bloc.dart';
+import 'package:shopyo/features/customer/profile/presentation/widgets/language_change.dart';
 import 'package:shopyo/features/customer/profile/presentation/widgets/user_profile_info.dart';
 import 'package:shopyo/features/customer/profile/presentation/widgets/user_profile_shimmer.dart';
 
@@ -14,6 +20,7 @@ class ProfileBody extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 10.h),
       child: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Center(
               child: BlocBuilder<ProfileBloc, ProfileState>(
@@ -27,6 +34,19 @@ class ProfileBody extends StatelessWidget {
                 },
               ),
             ),
+            SizedBox(height: 20.h),
+            CustomFadeInRight(
+              duration: 400,
+              child: TextApp(
+                text: context.translate(LangKeys.applicationFeatures),
+                theme: context.textStyle.copyWith(
+                  fontSize: 14.sp,
+                  fontWeight: FontWeightHelper.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 30.h),
+            CustomFadeInRight(duration: 400, child: LanguageChange()),
           ],
         ),
       ),
